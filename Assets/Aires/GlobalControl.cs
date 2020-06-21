1784b0604d5b8fcfa7e71336531507988b4ed910
Aires Pestana <airespt@gmail.com>
2020-06-21 19:08:51 +0100

    Bad ship control

---
+++ Assets/Aires/GlobalControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GlobalControl : MonoBehaviour
+{
+    public Collider2D Sun;
+    public Ship Player;
+
+    // public float AngDampen;
+    // public float AngSpdConstant;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player.angularSpeed -= AngDampen;
+        // if( Input.GetKey(KeyCode.LeftArrow) ) {
+        //     Player.angularSpeed -= AngSpdConstant;
+        // }
+        // if( Input.GetKey(KeyCode.RightArrow) ) {
+        //     Player.angularSpeed += AngSpdConstant;
+        // }
+
+        // FAIL
+        // if( Input.GetKey(KeyCode.LeftArrow) )
+        //     Player.angularSpeed = Player.AngSpdConstant;
+        // else
+        //     Player.angularSpeed = 0;
+        // if( Input.GetKey(KeyCode.RightArrow) )
+        //     Player.angularSpeed = -Player.AngSpdConstant;
+        // else
+        //     Player.angularSpeed = 0;
+
+        if( Input.GetKey(KeyCode.LeftArrow) )
+            Player.angularSpeed = Player.AngSpdConstant;
+        else if( Input.GetKey(KeyCode.RightArrow) )
+            Player.angularSpeed = -Player.AngSpdConstant;
+        else
+            Player.angularSpeed = 0;
+
+
+    }
+
+}
---
+++ Assets/Aires/Ship/Ship.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public Vector2 speed = new Vector2();
+    public float AngSpdConstant;
+
+    public float angularSpeed;
+
+    new private Collider2D collider;
+
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+
+    // }
+
+    void FixedUpdate() {
+        transform.Rotate(0, 0, angularSpeed);
+    }
+}
